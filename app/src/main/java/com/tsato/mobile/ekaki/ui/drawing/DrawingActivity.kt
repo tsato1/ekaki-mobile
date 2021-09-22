c5ff3ca59e5474c26486cd349d38429fede0e6d6
tsato1 <a8tb1117@gmail.com>
2021-09-22 15:22:28 +0800

    navigation drawer

--- app/src/main/java/com/tsato/mobile/ekaki/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/tsato/mobile/ekaki/ui/drawing/DrawingActivity.kt
@@ -1,11 +1,20 @@
 package com.tsato.mobile.ekaki.ui.drawing
 
+import android.graphics.Color
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import com.tsato.mobile.ekaki.R
 import com.tsato.mobile.ekaki.databinding.ActivityDrawingBinding
+import com.tsato.mobile.ekaki.util.Constants
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
@@ -16,22 +25,76 @@ class DrawingActivity : AppCompatActivity() {
 
     private val viewModel: DrawingViewModel by viewModels()
 
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var rvPlayers: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDrawingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         subscribeToUiStateUpdates()
 
+        toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+        toggle.syncState()
+
+        val header = layoutInflater.inflate(R.layout.nav_drawer_header, binding.navView)
+        rvPlayers = header.findViewById(R.id.rvPlayers)
+        binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED) // nav drawer closed
+
+        binding.ibPlayers.setOnClickListener {
+            binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            binding.root.openDrawer(GravityCompat.START)
+        }
+
+        binding.root.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) = Unit
+
+            override fun onDrawerOpened(drawerView: View) = Unit
+
+            override fun onDrawerClosed(drawerView: View) {
+                binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+
+            override fun onDrawerStateChanged(newState: Int) = Unit
+        })
+
         binding.colorGroup.setOnCheckedChangeListener { _, checkedId ->
             viewModel.checkRadioButton(checkedId)
         }
+    }
+
+    private fun selectColor(color: Int) {
+        binding.drawingView.setColor(color)
+        binding.drawingView.setThickness(Constants.DEFAULT_PAINT_THICKNESS)
     }
 
     private fun subscribeToUiStateUpdates() {
         lifecycleScope.launchWhenStarted {
             viewModel.selectedColorButtonId.collect { id ->
                 binding.colorGroup.check(id)
+
+                when (id) {
+                    R.id.rbBlack -> selectColor(Color.BLACK)
+                    R.id.rbBlue -> selectColor(Color.BLUE)
+                    R.id.rbGreen -> selectColor(Color.GREEN)
+                    R.id.rbOrange -> {
+                        selectColor(ContextCompat.getColor(this@DrawingActivity, R.color.orange))
+                    }
+                    R.id.rbRed -> selectColor(Color.RED)
+                    R.id.rbYellow -> selectColor(Color.YELLOW)
+                    R.id.rbEraser -> {
+                        binding.drawingView.setColor(Color.WHITE)
+                        binding.drawingView.setThickness(40f)
+                    }
+                }
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
